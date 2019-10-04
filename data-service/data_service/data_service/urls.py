1acef60714cc5dfdf2f93703ea177410d0a767d2
Haotian9850 <haotian9850@gmail.com>
2019-10-04 00:08:47 -0400

    updated data_service

--- data-service/data_service/data_service/urls.py
+++ data-service/data_service/data_service/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include, url
 from django.urls import path
+from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('test/get_all_pets', views.get_pet_list)
 ]
--- data-service/data_service/main/views.py
+++ data-service/data_service/main/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 import datetime
 from django import db
 from django.db import IntegrityError
+from django.http import JsonResponse
 from main import models 
 import json
 
+from services.services import get_all_pets
 
-# Create your views here.
+def get_pet_list(request):
+    return JsonResponse({
+        'ok': True,
+        'res': get_all_pets()
+    })
+
+
+
 
