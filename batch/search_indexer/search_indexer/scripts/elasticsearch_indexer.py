f6795462ae4fff25d2be56fac51afa87f2a04134
Haotian9850 <haotian9850@gmail.com>
2019-10-26 14:25:14 -0400

    updatde elasticsearch service

--- batch/search_indexer/search_indexer/scripts/elasticsearch_indexer.py
+++ batch/search_indexer/search_indexer/scripts/elasticsearch_indexer.py
@@ -7,6 +7,39 @@ import time
 import threading
 
 
+es_id_mapping
+
+
+def get_es_client():
+    es = Elasticsearch(["elasticsearch:9200"])
+    if not es.ping():
+        print("Cannot establish a connection to elasticsearch host.")
+    return es
+
+
+def init(index_name, index_mapping):
+    es = get_es_client()
+    if not check_existing_index(es, index_name):
+        return es.indices.create(
+            index=index_name,
+            body=index_mapping
+        )
+    
+
+def check_existing_index(es, index_name):
+    return es.indices.exists(index_name)
+
+
+# check for pet existence first
+'''
+def index_pet(message, index_name):
+    es = get_es_client()
+    return es.index(
+        index=index_name,
+        body=parse_pet(message)
+    )
+'''
+
 
 def print_consumer_topic():  
     consumer = KafkaConsumer(
@@ -26,6 +59,4 @@ def print_consumer_topic():
 
 
 
-def index_new_pet():
-    es = Elasticsearch(["elasticsearch"])
     
