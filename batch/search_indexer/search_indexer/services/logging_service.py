edb8fb4f87bb9a87c217601ca889a956c15e15be
Haotian9850 <haotian9850@gmail.com>
2019-10-28 23:47:22 -0400

    updated logging service

--- batch/search_indexer/search_indexer/services/logging_service.py
+++ batch/search_indexer/search_indexer/services/logging_service.py
@@ -10,14 +10,25 @@ def log_pet_views(view):
 
 def assemble_log_entry(view):
     return "{}:{}".format(
-        view["user_id"],
-        view["pet_id"]
+        view["pet_id"],
+        view["user_id"]
     )
 
 
+def parse_pet_log():
+    result = {}
+    with open("../pet_view_log.txt") as log:
+        lines = log.readlines()
+    for line in lines:
+        result[line.split(":")[2]] = result.get(line.split(":")[2], 0) + 1
+    print(result)
+    return result
+
+
+
+'''
 view = {
     "user_id": 29,
     "pet_id": 1
 }
-
-log_pet_views(view)
+'''
